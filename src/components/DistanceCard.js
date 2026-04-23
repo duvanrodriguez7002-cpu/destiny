0fe4e039664c1850ba4300db88abcd46f8d376ff
crZ-eng <suarezhurtadocristianferley@gmail.com>
2026-04-23 16:13:33 -0500

    distanceCard

---
+++ src/components/DistanceCard.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Text, StyleSheet, StatusBar } from 'react-native',
+
+
+const DistanceCard = ({distance}) => {
+    return (
+        <view Style={styles.Card}>
+            <Text style={styles.title}>Distancia</Text>
+            <Text style={styles.Distance}>
+                {distance ? `${distance.toFixed(2)} km` : '---'}
+            </Text>
+        </view>
+    );
+};
+
+export default DistanceCard;
+
+const styles = StyleSheet.create({
+    card:{
+        backgroundColor: '#1e1e1e',
+
+    },
+
+    title:{
+
+    },
+    distance:{
+
+    },
+
+})
